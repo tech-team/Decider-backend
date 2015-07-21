9aa476a3268a0532886ea291f883faf0ea481967
i.abashin <abashinos@gmail.com>
2015-07-21 20:07:48 +0300

    new share pic

--- decider_api/views/share_views.py
+++ decider_api/views/share_views.py
@@ -6,7 +6,6 @@ import os
 from oauth2_provider.views import ProtectedResourceView
 import re
 from decider_api.utils.endpoint_decorators import require_params
-from decider_api.utils.image_helper import IMAGE_SIZE
 from decider_app.models import Question, PollItem
 from decider_app.views.utils.response_builder import build_response, build_error_response
 from decider_app.views.utils.response_codes import CODE_CREATED, CODE_UNKNOWN_QUESTION
@@ -15,15 +14,16 @@ from decider_backend.settings import MEDIA_ROOT, STATIC_ROOT
 
 class ShareEndpoint(ProtectedResourceView):
 
-    BORDER_SIZE = 15
-    OFFSETS = ((78, 50), (1210, 50))
+    # BORDER_SIZE = 15
+    SHARE_IMAGE_SIZE = (360, 640)
+    OFFSETS = ((90, 44), (647, 44))
 
     @require_params(['question_id'])
     def post(self, request, *args, **kwargs):
         bg = Image.open(os.path.join(STATIC_ROOT, "img", "share.png"))
-        logo = Image.open(os.path.join(STATIC_ROOT, "img", "logo.png"))
-        logo_offset = ((self.OFFSETS[1][0] + self.OFFSETS[0][0] + IMAGE_SIZE[0] - logo.size[0])//2,
-                       self.OFFSETS[0][1] + IMAGE_SIZE[1] - logo.size[1])
+        # logo = Image.open(os.path.join(STATIC_ROOT, "img", "logo.png"))
+        # logo_offset = ((self.OFFSETS[1][0] + self.OFFSETS[0][0] + self.SHARE_IMAGE_SIZE[0] - logo.size[0])//2,
+        #                self.OFFSETS[0][1] + self.SHARE_IMAGE_SIZE[1] - logo.size[1])
 
         question_id = int(request.POST.get('question_id'))
 
@@ -31,15 +31,12 @@ class ShareEndpoint(ProtectedResourceView):
         if not pi:
             return build_error_response(httplib.NOT_FOUND, CODE_UNKNOWN_QUESTION, "Question unknown")
 
-        image_size = (IMAGE_SIZE[0] - self.BORDER_SIZE, IMAGE_SIZE[1] - self.BORDER_SIZE)
-        left_img = ImageOps.expand(Image.open(os.path.join(MEDIA_ROOT, re.sub("media/?", "", pi[0].picture.url)))
-                                   .resize(image_size), border=15, fill='grey')
-        right_img = ImageOps.expand(Image.open(os.path.join(MEDIA_ROOT, re.sub("media/?", "", pi[1].picture.url)))
-                                    .resize(image_size), border=15, fill='grey')
+        left_img = Image.open(os.path.join(MEDIA_ROOT, re.sub("media/?", "", pi[0].picture.url))).resize(self.SHARE_IMAGE_SIZE)
+        right_img = Image.open(os.path.join(MEDIA_ROOT, re.sub("media/?", "", pi[1].picture.url))).resize(self.SHARE_IMAGE_SIZE)
 
         bg.paste(left_img, self.OFFSETS[0])
         bg.paste(right_img, self.OFFSETS[1])
-        bg.paste(logo, logo_offset, logo)
+        # bg.paste(logo, logo_offset, logo)
 
         cur_time = timezone.now().strftime('%s')
         uid = uuid.uuid4().hex
